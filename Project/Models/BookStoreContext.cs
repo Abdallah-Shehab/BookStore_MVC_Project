fe55fb212009a9651b01ef92de96cc2a2a471165
Abdallah-Shehab <SHaFiQ@DESKTOP-78IHH49>
2024-03-20 00:56:27 +0200

    Empity database , first time deploy

---
+++ Project/Models/BookStoreContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.Emit;
+
+namespace Project.Models
+{
+    public class BookStoreContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+        }
+
+        public BookStoreContext() : base()
+        {
+
+        }
+
+        DbSet<Book> Books { get; set; }
+        DbSet<Author> Authors { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Comment> Comments { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<OrderDetails> OrdersDetails { get; set; }
+        DbSet<Discount> Discounts { get; set; }
+        DbSet<ApplicationUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<OrderDetails>().HasKey("Order_id", "Book_id");
+            builder.Entity<Comment>().HasKey("user_id", "book_id");
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ Project/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Models
+{
+    public class Comment
+    {
+
+
+        public string comment { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [ForeignKey("user")]
+        public int user_id { get; set; }
+
+        [ForeignKey("book")]
+        public int book_id { get; set; }
+        public ApplicationUser user { get; set; }
+        public Book book { get; set; }
+
+
+    }
+}
