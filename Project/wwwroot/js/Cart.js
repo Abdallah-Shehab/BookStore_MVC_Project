1ee241ba22493f34f4363e6f55f7b77013dc708e
Azza <azzagamel11@gmail.com>
2024-03-23 23:53:48 +0200

    add comments in cart.js file

--- Project/wwwroot/js/Cart.js
+++ Project/wwwroot/js/Cart.js
@@ -2,38 +2,37 @@
 
 function addToCart(id, text) {
     let quantity;
-    if (text == 'whithout quantity') {
+    if (text == 'whithout quantity') { // just click on (Add to cart) button without select any quantity --> from any page except BookDetails
         quantity = 1;
     }
-    else {
+    else { //comes from BookDetails page & take the quantity from the input
         quantity = document.getElementById("qty").value;
     }
-    let flag = false;
+    let flag = false; //to know if the book already exist and increse its quantity
 
     let arritems = JSON.parse(localStorage.getItem("cart") || "[]");
 
-    if (arritems.length == 0) {
+    if (arritems.length == 0) { //cart is empty
         setItem("cart", [{ "bookID": id, "quantity": quantity }]);
         toster();
         toastr.success("The Book is added to the Cart Successfully.");
     }
-    else {
-        console.log("else")
+    else {  //cart has items --> we have some options (add book for the first time / book exist so <update quantity> or <say this is alredy exist and no action>)
         let newitems = [];
         arritems.forEach(obj => {
-            if (obj.bookID == id) {
+            if (obj.bookID == id) {  //selected book is exist in the cart
                 flag = true;
                 toster();
-                if (text == 'whithout quantity') {
-                    toastr.warning("The Book is already exist");
+                if (text == 'whithout quantity') {   //comes from any page 
+                    toastr.warning("The Book is already exist");  
                 }
-                else {
+                else {  //from bookdetails to update the quantity
                     obj.quantity = quantity;
-                    toastr.success("The Book is already exist & update the quantity.");
+                    toastr.success("The Book is already exist & update the quantity.");  
                 }
             }
             newitems.push(obj);
-            if (newitems.length == arritems.length && flag == false) {
+            if (newitems.length == arritems.length && flag == false) {  //the cart has items but this selected book isn't exist -->(add book for the first time)
                 newitems.push({ "bookID": id, "quantity": quantity });
                 toster();
                 toastr.success("The Book is added to the Cart Successfully.");
