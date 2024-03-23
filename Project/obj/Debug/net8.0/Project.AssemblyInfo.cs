a6f74a66932986542a36d262282101aad73bd816
Mohamed <mohahamdy11@gmail.com>
2024-03-23 17:25:37 +0200

    Add services and create discount repo

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda7118e0f3b08dbeac4870972130056c710f477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb797cc2219a9aeb96f7ff9cc799feedff4fad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
