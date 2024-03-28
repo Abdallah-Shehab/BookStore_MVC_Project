54d729b33efbabddec4019d5a54dd3280d15f2b9
Hala Mansour Ali <halamansour888222555@gmail.com>
2024-03-28 10:01:35 +0200

    Admin Crud Operation (Display - Update - Delete)

---
+++ Project/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+using Project.Repositories;
+using Project.ViewModels;
+
+namespace Project.Controllers
+{
+    public class OrderController : Controller
+    {
+
+
+        private readonly ILogger<OrderController> _logger;
+        IBookRepository bookRepository;
+
+        BookStoreContext db;
+        public OrderController(ILogger<OrderController> logger, IBookRepository bookRepository, BookStoreContext db)
+        {
+            _logger = logger;
+            this.bookRepository = bookRepository;
+
+
+
+
+        }
+
+        public IActionResult Cart()
+        {
+            return View("Cart");
+        }
+
+
+       
+
+    }
+}
