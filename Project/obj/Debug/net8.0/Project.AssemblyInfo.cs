2fa191091fa1f2b64cb629bb6a9f534077093010
Hala Mansour Ali <halamansour888222555@gmail.com>
2024-03-31 03:55:51 +0200

    Order and Add Admin Added

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f7f33683b08bfb2cfd1ac065b7cbb5cfcd7d6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
